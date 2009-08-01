ba2fe3d650d66c232387a756b136d5236ef20712
gerstrong <gerstrong@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2009-08-01 19:40:21 +0000

    CK8.4 Adation of Enemy AI
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@202 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

---
+++ src/ai/earth.h
@@ -0,0 +1,13 @@
+/*
+ * earth.h
+ *
+ *  Created on: 01.08.2009
+ *      Author: gerstrong
+ */
+
+#ifndef EARTH_H_
+#define EARTH_H_
+
+void delete_object(int o);
+
+#endif /* EARTH_H_ */
---
+++ src/ai/fireball.h
@@ -0,0 +1,13 @@
+/*
+ * fireball.h
+ *
+ *  Created on: 01.08.2009
+ *      Author: gerstrong
+ */
+
+#ifndef FIREBALL_H_
+#define FIREBALL_H_
+
+void delete_object(int o);
+
+#endif /* FIREBALL_H_ */
