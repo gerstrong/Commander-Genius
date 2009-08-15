d90cf3c88aa64adc626481bea468941991a4aefc
albertzeyer <albertzeyer@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2009-08-15 13:19:09 +0000

    script for creating MacOSX DMG
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@243 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

---
+++ build/Xcode/createdmg.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# script taken from Pidgin-MacOSX project
+# by Albert Zeyer - www.az2000.de
+
+# builds a DMG for Commander Genius
+
+
+# Creates a disk image (dmg) on Mac OS X from the command line.
+# usage:
+#    mkdmg <volname> <vers> <srcdir>
+#
+# Where <volname> is the name to use for the mounted image, <vers> is the version
+# number of the volume and <srcdir> is where the contents to put on the dmg are.
+#
+# The result will be a file called <volname>-<vers>.dmg
+mkdmg() {
+
+	if [ $# != 3 ]; then
+	 echo "usage: mkdmg.sh volname vers srcdir"
+	 exit 0
+	fi
+
+	VOL="$1"
+	VER="$2"
+	FILES="$3"
+
+	DMG="tmp-$VOL.dmg"
+
+	# create temporary disk image and format, ejecting when done
+	SIZE=$(du -sk ${FILES} | cut -f 1)
+	SIZE=$((${SIZE}/1000+5))
+	hdiutil create "$DMG" -megabytes ${SIZE} -ov -type UDIF -fs HFS+ -volname "$VOL"
+
+	# mount and copy files onto volume
+	hdid "$DMG"
+	echo -n "copying files ... "
+	cp -R "${FILES}"/* "/Volumes/$VOL/"
+	echo "ready"
+	hdiutil eject "/Volumes/$VOL"
+	#osascript -e "tell application "Finder" to eject disk "$VOL"" && 
+
+	# convert to compressed image, delete temp image
+	rm -f "${VOL}-${VER}.dmg"
+	hdiutil convert "$DMG" -format UDZO -o "${VOL}-${VER}.dmg"
+	rm -f "$DMG"
+
+}
+
+
+cd "$(dirname "$0")"
+rsync -a --delete build/Debug/Commander\ Genius.app dmg/
+mkdmg CommanderGenius "$(egrep -E ".*#define.*CGVERSION.*" ../../version.h | cut -d "\"" -f 2)" dmg
