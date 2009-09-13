2163c3b7719484c9b2dcaa15518fd1d198233c0f
gerstrong <gerstrong@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2009-09-13 10:38:59 +0000

    Updated cmake structure. Now 32-bit and 64-bit Linux can be be build. Windows with mingw32 is the next to be adapted!
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@391 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

--- build/Windows/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmingw32 -lSDLmain -lSDL -lglu32 -lopengl32 -lvorbisfile -lvorbis -logg
