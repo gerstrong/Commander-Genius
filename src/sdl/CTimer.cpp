0cf556640668cda48503dd889c3e375ced0d152b
Pickle136 <Pickle136@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2009-10-29 02:05:48 +0000

    Added sync feature to timer, removed chunk feature
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@532 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

--- src/sdl/CTimer.cpp
+++ src/sdl/CTimer.cpp
@@ -9,11 +9,7 @@
 This timer calculates a common factor between the desired rates for logic and render. By having this
 common rate all of the time during one second can be divided into a measurement scale where both
 other rates can be triggered.
-A chunk factor is also used, this group a number of loops togther before delaying. The time normally
-between these loops is minimal and the user does not notice if the loops are performed one after
-enough. A delay is then performed after the loops are been executed to keep the chunk in sync.
-The longer delay makes the needed accuracy of the timing system significantly less.
-This also improves accuracy of the loops.
+A sync rate is used to sync a group of frames exactly to the desired frame rate.
 
 Examples:
 NORMAL RATES
@@ -21,21 +17,21 @@ Frame Rate = 60
 Logic Rate = 60
 
 Loops Needed = 60
-Normal duration: 16.66 ms Chunk of 3 duration: 50 ms
+Normal duration: 16.66 ms Sync of 3 duration: 50 ms
 
 LOW RATES
 Frame Rate = 20 (interval of 3)
 Logic Rate = 60
 
 Loops Needed = 60
-Normal duration: 16.66 ms Chunk of 3 duration: 50 ms
+Normal duration: 16.66 ms Sync of 3 duration: 50 ms
 
 HIGH RATES
 Frame Rate = 120
 Logic Rate = 60 (interval of 2)
 
 Loops Needed = 120
-Normal duration: 8.33 ms Chunk of 3 duration: 25 ms
+Normal duration: 8.33 ms Sync of 3 duration: 25 ms
 
 */
 
@@ -49,10 +45,10 @@ Normal duration: 8.33 ms Chunk of 3 duration: 25 ms
 CTimer::CTimer()
 {
 	m_LoopPS = m_LPS = m_FPS = 0;
-	m_ChunkCount = m_LoopCount = m_LogicCount = m_FrameCount = 0;
+	m_SyncCount = m_LoopCount = m_LogicCount = m_FrameCount = 0;
 
-	setFrameRate(DEFAULT_LPS, DEFAULT_FPS, DEFAULT_CHUNK);
-    m_LoopStartTime = SDL_GetTicks();
+	setFrameRate(DEFAULT_LPS, DEFAULT_FPS, DEFAULT_SYNC);
+    m_SyncStartTime = m_LoopStartTime = SDL_GetTicks();
 	g_pLogFile->textOut(GREEN, true, "Starting timer driver...\n");
 }
 
@@ -60,12 +56,12 @@ CTimer::~CTimer()
 {
 }
 
-void CTimer::setFrameRate( int logicrate, int framerate, int chunkrate )
+void CTimer::setFrameRate( int logicrate, int framerate, int syncrate )
 {
     int looprate, factor;
 
     // Set all of the desired rates
-    m_ChunkRate = chunkrate;
+    m_SyncRate  = syncrate;
     m_LogicRate = logicrate;
 	m_FrameRate = framerate;
 
@@ -98,14 +94,19 @@ void CTimer::setFrameRate( int logicrate, int framerate, int chunkrate )
 
 void CTimer::CalculateIntervals( void )
 {
+    float looprate;
+
     // Determine the number of loops needed for each logic and frame loop rates
     m_LogicInterval = m_LoopRate / m_LogicRate;
     m_FrameInterval = m_LoopRate / m_FrameRate;
-    // Calculate the amount of time each loop chunk should last (floats keep from losing accuracy with integers)
-    m_LoopDuration  = (int)(((float)MSPERSEC / (float)m_LoopRate) * (float)m_ChunkRate);
+    // Calculate the amount of time each loop Sync should last (floats keep from losing accuracy with integers)
+    looprate = (float)MSPERSEC / (float)m_LoopRate;
+    m_LoopDuration  = (int)looprate;
+    looprate = looprate * (float)m_SyncRate;
+    m_SyncDuration  = (int)looprate;
 
     //printf( "LoopRate %d LogicRate %d FrameRate %d\n", m_LoopRate, m_LogicRate, m_FrameRate );
-    //printf( "LogicInt %d FrameInt %d LoopDur %d\n", m_LogicInterval, m_FrameInterval, m_LoopDuration );
+    //printf( "LogicInt %d FrameInt %d LoopDur %d SyncDur %d\n", m_LogicInterval, m_FrameInterval, m_LoopDuration, m_SyncDuration );
 }
 
 bool CTimer::TimeToLogic( void )
@@ -140,17 +141,26 @@ void CTimer::TimeToDelay( void )
 	ulong curtime = SDL_GetTicks();
 
     m_LoopCount++;
-    m_ChunkCount++;
+    m_SyncCount++;
 
-    // If the chunk rate is met, check time took
-    if (m_ChunkCount>=m_ChunkRate)
+    // If the Sync rate is met, check time took
+    if (m_SyncCount>=m_SyncRate)
+    {
+        // Delay for the remaining time
+        delay = m_SyncDuration - (int)(curtime - m_SyncStartTime);
+        if (delay>0) {
+            SDL_Delay(delay);
+        }
+        m_SyncCount = 0;
+        m_SyncStartTime = m_LoopStartTime = SDL_GetTicks();
+    }
+    else
     {
         // Delay for the remaining time
         delay = m_LoopDuration - (int)(curtime - m_LoopStartTime);
         if (delay>0) {
             SDL_Delay(delay);
         }
-        m_ChunkCount = 0;
         m_LoopStartTime = SDL_GetTicks();
     }
 
@@ -163,7 +173,7 @@ void CTimer::TimeToDelay( void )
 		m_LoopCount = m_LogicCount = m_FrameCount = 0;
 		m_FPSCountTime  = curtime;
 
-        //printf( "LoopPS %d LPS %d FPS %d\n", m_LoopPS, m_LPS, m_FPS );
+        printf( "LoopPS %d LPS %d FPS %d\n", m_LoopPS, m_LPS, m_FPS );
 	}
 }
 
--- src/sdl/CTimer.h
+++ src/sdl/CTimer.h
@@ -13,7 +13,7 @@
 #define g_pTimer	CTimer::Get()
 
 #define MSPERSEC        1000
-#define DEFAULT_CHUNK   3
+#define DEFAULT_SYNC    3
 #define DEFAULT_LPS     60
 #define DEFAULT_FPS     60
 
@@ -35,7 +35,7 @@ public:
 
 	int getLogicRate() { return m_LogicRate; }
 	int getFrameRate() { return m_FrameRate; }
-	void setFrameRate( int logicrate, int framerate, int chunkrate );
+	void setFrameRate( int logicrate, int framerate, int syncrate );
 
     int getLogicPerSec( void ) { return m_LPS; }
 	int getFramesPerSec( void ) { return m_FPS; }
@@ -44,11 +44,11 @@ private:
     int m_LPS, m_LogicCount, m_LogicRate, m_LogicInterval;
     int m_FPS, m_FrameCount, m_FrameRate, m_FrameInterval;
     int m_LoopPS, m_LoopRate, m_LoopCount, m_LoopDuration;
-    int m_ChunkCount, m_ChunkRate;
+    int m_SyncCount, m_SyncRate, m_SyncDuration;
 
 
     ulong m_LPSCountTime, m_FPSCountTime;
-	ulong m_LoopStartTime;
+	ulong m_LoopStartTime, m_SyncStartTime;
 	ulong m_LastSecTime;
 };
 
