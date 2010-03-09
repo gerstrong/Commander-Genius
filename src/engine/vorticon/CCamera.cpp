7f3f318dace52c2b32a4958f546f401c1d173d10
gerstrong <gerstrong@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2010-03-09 20:53:44 +0000

    Added camera class
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@919 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

---
+++ src/engine/vorticon/CCamera.cpp
@@ -0,0 +1,19 @@
+/*
+ * CCamera.cpp
+ *
+ *  Created on: 09.03.2010
+ *      Author: pizza2004
+ */
+
+#include "CCamera.h"
+
+CCamera::CCamera()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CCamera::~CCamera()
+{
+	// TODO Auto-generated destructor stub
+}
---
+++ src/engine/vorticon/CCamera.h
@@ -0,0 +1,18 @@
+/*
+ * CCamera.h
+ *
+ *  Created on: 09.03.2010
+ *      Author: pizza2004
+ */
+
+#ifndef CCAMERA_H_
+#define CCAMERA_H_
+
+class CCamera
+{
+public:
+	CCamera();
+	virtual ~CCamera();
+};
+
+#endif /* CCAMERA_H_ */
