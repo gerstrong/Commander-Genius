b0e702b3bf4cef81dfd295c15f4a4077041f2981
albertzeyer <albertzeyer@4df4b0f3-56ce-47cb-b001-ed939b7d65a6>
2010-03-21 12:29:06 +0000

    removing wrong code (all such checks for __APPLE__ are *always* wrong)
    git-svn-id: https://clonekeenplus.svn.sourceforge.net/svnroot/clonekeenplus/cgenius/trunk@942 4df4b0f3-56ce-47cb-b001-ed939b7d65a6

--- src/engine/CEGAGraphics.cpp
+++ src/engine/CEGAGraphics.cpp
@@ -25,11 +25,7 @@ bool CEGAGraphics::loadData()
 	Font.DestroySurface();
 	Font.CreateSurface( g_pGfxEngine->Palette.m_Palette, SDL_SWSURFACE, g_pVideoDriver->getDepth() );
 	Font.optimizeSurface();
-	#if defined(__APPLE__)
-	std::string path = GetFullFileName("res/gfx/menufonts.bmp");
-	#else
 	std::string path = GetFullFileName("data/res/gfx/menufonts.bmp");
-#endif
 	if( Font.loadHiColourFont(path) )
 		g_pLogFile->textOut(GREEN, "VGA Fontmap for the gamemenu has been loaded successfully!");
 
