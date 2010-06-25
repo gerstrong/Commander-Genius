385984995424a4d819230c12699de9a983437dab
Gerhard Stein <gerstrong@gmail.com>
2010-06-25 17:05:35 +0200

    Corrected Version number

--- version.h
+++ version.h
@@ -1,11 +1,7 @@
 #ifndef __CG__VERSION_H__
 #define __CG__VERSION_H__
 
-<<<<<<< HEAD
-#define CGVERSION	"0314"
-=======
-#define CGVERSION	"0.3"
->>>>>>> b9a21afe07cc5075e242a882054d9a396830f844
+#define CGVERSION	"0.3.2"
 
 #endif
 
