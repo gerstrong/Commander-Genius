b099df433fb76ae8fe67f0b91b538b83b298d60b
Gerhard Stein <gerstrong@gmail.com>
2010-07-23 00:21:05 +0200

    fixed a small door bug

--- src/common/CPlayerLevel.cpp
+++ src/common/CPlayerLevel.cpp
@@ -63,6 +63,17 @@ void CPlayer::processInLevel()
 			playpushed();
 		}
 
+		// Check left and right blocks again, because sometimes a door or mangling machine arm move away
+		if(!godmode)
+		{
+			int mlx = (getXLeftPos()-1)>>CSF;
+			int mrx = (getXRightPos()+1)>>CSF;
+			int my = getYUpPos()>>CSF;
+			std::vector<CTileProperties> &Tile = g_pBehaviorEngine->getTileProperties(1);
+			blockedr = Tile[mp_Map->at(mrx,my,1)].bleft;
+			blockedl = Tile[mp_Map->at(mlx,my,1)].bright;
+		}
+
 		checkSolidDoors();
 
 		InertiaAndFriction_X();
