1dbd40aa5b3c2e9f65f84be760a66029e5a99b73
Gerhard Stein <gerstrong@gmail.com>
2010-08-03 08:38:26 +0200

    Added new Event classes which hopefully might reduce the code

---
+++ src/engine/CEvent.cpp
@@ -0,0 +1,17 @@
+/*
+ * CEvent.cpp
+ *
+ *  Created on: 03.08.2010
+ *      Author: gerstrong
+ */
+
+#include "CEvent.h"
+
+CEvent::CEvent() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CEvent::~CEvent() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/engine/CEvent.h
@@ -0,0 +1,22 @@
+/*
+ * CEvent.h
+ *
+ *  Created on: 03.08.2010
+ *      Author: gerstrong
+ *
+ *  This is a new Event class.
+ *  The events which are desired
+ *  to be used in some section of the
+ *  code must be declared through a enum
+ */
+
+#ifndef CEVENT_H_
+#define CEVENT_H_
+
+class CEvent {
+public:
+	CEvent();
+	virtual ~CEvent();
+};
+
+#endif /* CEVENT_H_ */
