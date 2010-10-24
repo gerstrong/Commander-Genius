22ad888cc56f48ca301e6c3f5f59851f0e1a0782
Gerhard Stein <gerstrong@gmail.com>
2010-10-24 12:50:11 +0200

    more adaptations

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -88,7 +88,7 @@ int ThreadPool::threadWrapper(void* param) {
 		SDL_mutexV(data->pool->mutex);
 		
 		SDL_CondSignal(data->pool->threadStartedWork);
-		g_pLogFile->textOut("Running Thread: " + data->name + " ... ");
+		g_pLogFile->textOut("Running Thread: " + data->name);
 		data->ret = act->handle();
 		delete act;
 		g_pLogFile->textOut( data->name + " [finished]" );
