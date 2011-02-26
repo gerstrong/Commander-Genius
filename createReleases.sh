0babe72f7902909da8b0e664e0363b84e61c7e8a
Gerhard Stein <gerstrong@gmail.com>
2011-02-26 23:54:07 +0100

    added website path for our project

--- createReleases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-NAME=cgenius
-VERSION=0330
-
-#!/bin/sh
-
-sh createVersion.sh ${NAME}-v${VERSION}
-
--- createTestVersions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# put date on the filename
-dt=`date +%b%d%y-%H%M`
-
-sh createVersion.sh CG-Test-${dt}
