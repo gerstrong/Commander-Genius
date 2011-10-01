fdbba585119391936aa281356b7241dba4bdb07b
Gerstrong <gerstorng@gmail.com>
2011-10-01 15:58:10 +0200

    Scaler declared as class, which later will be an object of CVideoEngine

---
+++ src/sdl/video/scalers/CScaler.cpp
@@ -0,0 +1,18 @@
+/*
+ * CScaler.cpp
+ *
+ *  Created on: 01.10.2011
+ *      Author: gerstrong
+ */
+
+#include "CScaler.h"
+
+namespace galaxy {
+
+CScaler::CScaler()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace galaxy */
---
+++ src/sdl/video/scalers/CScaler.h
@@ -0,0 +1,27 @@
+/*
+ * CScaler.h
+ *
+ *  Created on: 01.10.2011
+ *      Author: gerstrong
+ */
+
+#ifndef CSCALER_H_
+#define CSCALER_H_
+
+namespace galaxy
+{
+
+class CScaler
+{
+
+public:
+	CScaler();
+
+private:
+
+	float wFac;
+	float hFac;
+};
+
+} /* namespace galaxy */
+#endif /* CSCALER_H_ */
