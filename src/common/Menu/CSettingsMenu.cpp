ae3391caf03fee2759ea7ed2fb3688a98225f80a
Chad Anderson <pizza29965@neejean.org>
2013-03-03 20:18:00 -0700

    Revert "Galaxy Controls Menu Fix"
    This reverts commit 01fa7276515d7d7e2d80d4da5a6a0d30ab6ea7ab.

--- src/common/Menu/CSettingsMenu.cpp
+++ src/common/Menu/CSettingsMenu.cpp
@@ -32,18 +32,27 @@ CBaseMenu( CRect<float>(0.25f, 0.24f, 0.5f, 0.32f) )
 	mpMenuDialog->addControl(new CGUIButton( "Options",
 									new OpenMenuEvent( new COptions() ) ) );
 
-    if (g_pBehaviorEngine->mPlayers == 1)
-    {
-        mpMenuDialog->addControl(new CGUIButton( "Controls",
-									    new OpenMenuEvent( new CControlsettings(1) ) ) );
-    } else {
-        mpMenuDialog->addControl(new CGUIButton( "Controls",
-									    new OpenMenuEvent( new CPlayersSelection<OpenControlMenuEvent>() ) ) );
-    }
+
+	/*
+
+			const int players = ctrlMenu->mSelection;
+			EventContainer.pop_Event();
+			EventContainer.add( new OpenMenuEvent(
+									new CControlsettings(players) ) );
+	 *
+	 * */
+
+#if defined (SINGLEPLAYER)
+	mpMenuDialog->addControl( new CGUIButton( "Controls",
+									new OpenMenuEvent(	new CControlsettings(1) ) ) );
+#else
+	mpMenuDialog->addControl(new CGUIButton( "Controls",
+									new OpenMenuEvent( new CPlayersSelection<OpenControlMenuEvent>() ) ) );
+#endif
 
 
 #if !defined(EMBEDDED)
-	mpMenuDialog->addControl(new CGUIButton( "Camera",
+	mpMenuDialog->addControl( new CGUIButton( "Camera",
 									new OpenMenuEvent( new CCameraSettings() ) ) );
 #endif
 
