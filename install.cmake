f1f09088651d5f98fd4c0332baf4d71c3fcc2382
hasufell <julian.ospald@googlemail.com>
2013-05-02 13:54:29 +0200

    INSTALL: make paths modifiable
    SHAREDIR -> pixmaps/desktop-files
    GAMES_SHAREDIR -> parent dir of "commandergenius" data dir
    DOCDIR -> docs

--- install.cmake
+++ install.cmake
@@ -2,11 +2,17 @@
 ############### Prepare the installation of the compiled stuff here!
 
 IF(BUILD_TARGET STREQUAL WIN32)
-SET(SHAREDIR CGenius)
+SET(DATADIR CGenius)
 SET(APPDIR CGenius)
 ELSE(BUILD_TARGET STREQUAL WIN32)
-SET(SHAREDIR share/CommanderGenius)
-SET(APPDIR games)
+SET(SHAREDIR share CACHE PATH "System share dir location")
+# seperate SHAREDIR and GAMES_SHAREDIR
+# for cases where we want /usr/share/games for the data
+# and /usr/share for desktop-files/pixmaps and such
+SET(GAMES_SHAREDIR "${SHAREDIR}/games" CACHE PATH "Game data root dir")
+SET(DATADIR "${GAMES_SHAREDIR}/commandergenius")
+SET(APPDIR games CACHE PATH "Binary destination")
+SET(DOCDIR ${DATADIR} CACHE PATH "Docs destination")
 ENDIF(BUILD_TARGET STREQUAL WIN32)
 
 # This will install the application itself
@@ -15,15 +21,15 @@ INSTALL(TARGETS CommanderGenius
 
 # This will copy the resources files to the proper directory
 INSTALL(DIRECTORY vfsroot/ 
-	DESTINATION ${SHAREDIR})
+	DESTINATION ${DATADIR})
 
 # This will copy the readme file. 
 INSTALL(FILES README 
-	DESTINATION ${SHAREDIR})
+	DESTINATION ${DOCDIR})
 
 # This will copy the readme file. 
 INSTALL(FILES changelog.txt 
-	DESTINATION ${SHAREDIR})
+	DESTINATION ${DOCDIR})
 
 # Windows might not have those dlls so we ship them with the packages
 IF(BUILD_TARGET STREQUAL WIN32)
