71fefcf89e25c3b8fffb5f8398e90f7ec2dcc2d3
Gerhard Stein <gerstrong@gmail.com>
2013-09-16 17:41:05 +0200

    Sparky AI fixes

--- src/engine/galaxy/ep5/ai/CSparky.cpp
+++ src/engine/galaxy/ep5/ai/CSparky.cpp
@@ -232,7 +232,7 @@ void CSparky::getTouchedBy(CSpriteObject &theObject)
 
 int CSparky::checkSolidD( int x1, int x2, int y2, const bool push_mode )
 {
-  if(getActionNumber(A_SPARKY_WALK))
+  //if(getActionNumber(A_SPARKY_WALK))
   {
 	if(turnAroundOnCliff( x1, x2, y2 ))
 	  setAction(A_SPARKY_TURN);
