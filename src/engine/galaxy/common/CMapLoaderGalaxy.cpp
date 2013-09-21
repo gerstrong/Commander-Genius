38acb2205dd7e801d7f07d29e88a6a4ff70870f9
Gerhard Stein <gerstrong@gmail.com>
2013-09-21 11:56:24 +0200

    More fixes and cleanups

--- src/engine/galaxy/common/CMapLoaderGalaxy.cpp
+++ src/engine/galaxy/common/CMapLoaderGalaxy.cpp
@@ -229,102 +229,100 @@ bool CMapLoaderGalaxy::loadMap(CMap &Map, Uint8 level)
   std::ifstream MapFile;
   if(OpenGameFileR(MapFile, getResourceFilename(gamemapfile,path,true,false), std::ios::binary))
   {
-    if(level_offset != 0)
+    if(level_offset == 0 && mapHeadContainer.empty())
     {
-      // Then jump to that location and read the level map data
-      MapFile.seekg (level_offset, std::ios::beg);
-      
-      int headbegin;
-      
-      // Get the level plane header
-      if(gotoNextSignature(MapFile))
-      {
-	/*
-	 *			  Plane Offsets:  Long[3]   Offset within GAMEMAPS to the start of the plane.  The first offset is for the background plane, the
-	 *                           second for the foreground plane, and the third for the info plane (see below).
-	 *			  Plane Lengths:  Word[3]   Length (in bytes) of the compressed plane data.  The first length is for the background plane, the
-	 *                           second for the foreground plane, and the third for the info plane (see below).
-	 *			  Width:          Word      Level width (in tiles).
-	 *			  Height:         Word      Level height (in tiles).  Together with Width, this can be used to calculate the uncompressed
-	 *										size of plane data, by multiplying Width by Height and multiplying the result by sizeof(Word).
-	 *			  Name:           Byte[16]  Null-terminated string specifying the name of the level.  This name is used only by TED5, not by Keen.
-	 *			  Signature:      Byte[4]   Marks the end of the Level Header.  Always "!ID!".
-	 */
-          int jumpback = 3*sizeof(longword) + 3*sizeof(word) +
-                  2*sizeof(word) + 16*sizeof(byte) + 4*sizeof(byte);
+        MapFile.close();
+        g_pLogFile->textOut("This Level doesn't exist in GameMaps");
+        return false;
+    }
 
-          headbegin = static_cast<int>(MapFile.tellg()) - jumpback;
-      }
-      else
-      {
-          MapFile.clear();
-          headbegin =  level_offset;
-      }
-      
-      MapFile.seekg( headbegin, std::ios_base::beg);
-      
-      // Get the header of level data
-      longword Plane_Offset[3];
-      longword Plane_Length[3];
-      word Width, Height;
-      char name[17];
-      
-      // Get the plane offsets
-      Plane_Offset[0] = fgetl(MapFile);
-      Plane_Offset[1] = fgetl(MapFile);
-      Plane_Offset[2] = fgetl(MapFile);
-      
-      // Get the dimensions of the level
-      Plane_Length[0] = fgetw(MapFile);
-      Plane_Length[1] = fgetw(MapFile);
-      Plane_Length[2] = fgetw(MapFile);
-      
-      Width = fgetw(MapFile);
-      Height = fgetw(MapFile);
-      
-      for(int c=0 ; c<16 ; c++)
-      {
-        name[c] = MapFile.get();
-      }
-      name[16] = '\0';
-      
-      // Get and check the signature
-      g_pLogFile->textOut("Loading the Level \"" + static_cast<std::string>(name) + "\" (Level No. "+ itoa(level) + ")<br>" );
-      Map.setLevelName(name);
-      
-      mLevelName = name;
-      
-      // Then decompress the level data using rlew and carmack
-      g_pLogFile->textOut("Decompressing the Map...<br>" );
-      
-      // Start with the Background
-      Map.createEmptyDataPlane(0, Width, Height);
-      Map.createEmptyDataPlane(1, Width, Height);
-      Map.createEmptyDataPlane(2, Width, Height);
-      
-      unpackPlaneData(MapFile, Map, 0, Plane_Offset[0], Plane_Length[0], magic_word);
-      unpackPlaneData(MapFile, Map, 1, Plane_Offset[1], Plane_Length[1], magic_word);
-      unpackPlaneData(MapFile, Map, 2, Plane_Offset[2], Plane_Length[2], magic_word);
-      
-      
-      Map.collectBlockersCoordiantes();
-      MapFile.close();
-      
-      // Now that we have all the 3 planes (Background, Foreground, Foes) unpacked...
-      // We only will show the first two of them in the screen, because the Foes one
-      // is the one which will be used for spawning the foes (Keen, platforms, enemies, etc.)
-      spawnFoes(Map);
+    // Then jump to that location and read the level map data
+    MapFile.seekg (level_offset, std::ios::beg);
+
+    int headbegin;
+
+    // Get the level plane header
+    if(gotoNextSignature(MapFile))
+    {
+        /*
+     *			  Plane Offsets:  Long[3]   Offset within GAMEMAPS to the start of the plane.  The first offset is for the background plane, the
+     *                           second for the foreground plane, and the third for the info plane (see below).
+     *			  Plane Lengths:  Word[3]   Length (in bytes) of the compressed plane data.  The first length is for the background plane, the
+     *                           second for the foreground plane, and the third for the info plane (see below).
+     *			  Width:          Word      Level width (in tiles).
+     *			  Height:         Word      Level height (in tiles).  Together with Width, this can be used to calculate the uncompressed
+     *										size of plane data, by multiplying Width by Height and multiplying the result by sizeof(Word).
+     *			  Name:           Byte[16]  Null-terminated string specifying the name of the level.  This name is used only by TED5, not by Keen.
+     *			  Signature:      Byte[4]   Marks the end of the Level Header.  Always "!ID!".
+     */
+        int jumpback = 3*sizeof(longword) + 3*sizeof(word) +
+                2*sizeof(word) + 16*sizeof(byte) + 4*sizeof(byte);
+
+        headbegin = static_cast<int>(MapFile.tellg()) - jumpback;
     }
     else
     {
-      MapFile.close();
-      g_pLogFile->textOut("This Level doesn't exist in GameMaps");
-      return false;
+        MapFile.clear();
+        headbegin =  level_offset;
     }
+
+    MapFile.seekg( headbegin, std::ios_base::beg);
+
+    // Get the header of level data
+    longword Plane_Offset[3];
+    longword Plane_Length[3];
+    word Width, Height;
+    char name[17];
+
+    // Get the plane offsets
+    Plane_Offset[0] = fgetl(MapFile);
+    Plane_Offset[1] = fgetl(MapFile);
+    Plane_Offset[2] = fgetl(MapFile);
+
+    // Get the dimensions of the level
+    Plane_Length[0] = fgetw(MapFile);
+    Plane_Length[1] = fgetw(MapFile);
+    Plane_Length[2] = fgetw(MapFile);
+
+    Width = fgetw(MapFile);
+    Height = fgetw(MapFile);
+
+    for(int c=0 ; c<16 ; c++)
+    {
+        name[c] = MapFile.get();
+    }
+    name[16] = '\0';
+
+    // Get and check the signature
+    g_pLogFile->textOut("Loading the Level \"" + static_cast<std::string>(name) + "\" (Level No. "+ itoa(level) + ")<br>" );
+    Map.setLevelName(name);
+
+    mLevelName = name;
+
+    // Then decompress the level data using rlew and carmack
+    g_pLogFile->textOut("Decompressing the Map...<br>" );
+
+    // Start with the Background
+    Map.createEmptyDataPlane(0, Width, Height);
+    Map.createEmptyDataPlane(1, Width, Height);
+    Map.createEmptyDataPlane(2, Width, Height);
+
+    unpackPlaneData(MapFile, Map, 0, Plane_Offset[0], Plane_Length[0], magic_word);
+    unpackPlaneData(MapFile, Map, 1, Plane_Offset[1], Plane_Length[1], magic_word);
+    unpackPlaneData(MapFile, Map, 2, Plane_Offset[2], Plane_Length[2], magic_word);
+
+
+    Map.collectBlockersCoordiantes();
+    MapFile.close();
+
+    // Now that we have all the 3 planes (Background, Foreground, Foes) unpacked...
+    // We only will show the first two of them in the screen, because the Foes one
+    // is the one which will be used for spawning the foes (Keen, platforms, enemies, etc.)
+    spawnFoes(Map);
   }
   else
   {
-    return false;
+      return false;
   }
 
 
