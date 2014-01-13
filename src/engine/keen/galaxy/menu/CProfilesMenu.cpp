4a7f7bdad39adc2905691e4ac0dd8d4054936c10
Gerhard Stein <gerstrong@gmail.com>
2014-01-13 17:59:50 +0100

    Selection is a template. Can be moved the Keenengine

--- src/engine/keen/galaxy/menu/CProfilesMenu.cpp
+++ src/engine/keen/galaxy/menu/CProfilesMenu.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "CProfilesMenu.h"
-#include "common/Menu/CSelectionMenu.h"
+#include "engine/keen/menu/CSelectionMenu.h"
 #include "common/CSettings.h"
 #include <base/video/CVideoDriver.h>
 #include <lib/base/GsTimer.h>
