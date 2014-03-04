b18f8e3733a6da039f7292e82af8b99b2039f835
Gerhard Stein <gerstrong@gmail.com>
2014-03-04 18:00:35 +0100

    Mimrock AI improved

--- src/engine/keen/galaxy/ep4/ai/CMimrock.cpp
+++ src/engine/keen/galaxy/ep4/ai/CMimrock.cpp
@@ -79,47 +79,54 @@ void CMimrock::getTouchedBy(CSpriteObject &theObject)
 bool CMimrock::isNearby(CSpriteObject &theObject)
 {
     if(dead || theObject.dead || mTimer > 0 )
-	return true;    
+        return true;
 
     if( !blockedd || getActionStatus(A_MIMROCK_JUMP) || getActionStatus(A_MIMROCK_BOUNCE) )
-	return true;    
-        
+        return true;
+
     if( CPlayerBase *player = dynamic_cast<CPlayerBase*>(&theObject) )
-    {	
-	    const int dx = player->getXMidPos() - getXMidPos();
-	    
-	    if( dx>-CSF_DISTANCE_TO_FOLLOW_TOLERANCE &&
-		dx<+CSF_DISTANCE_TO_FOLLOW_TOLERANCE )
-	    {
-		if( dx<0 )
-		    xDirection = LEFT;
-		else
-		    xDirection = RIGHT;
-		
-		if( dx>-CSF_DISTANCE_TO_JUMP_TOLERANCE &&
-		    dx<+CSF_DISTANCE_TO_JUMP_TOLERANCE )
-		{
-		    if( xDirection == -player->xDirection )
-		    {
-			setAction(A_MIMROCK_SIT);
-		    }
-		    else
-		    {		    
-			yinertia = -JUMP_HEIGHT;
-			mTimer = JUMP_TIME;
-			setAction(A_MIMROCK_JUMP);
-		    }
-		}
-		else
-		{
-		    if( xDirection == player->xDirection && !blockedr && !blockedl )
-			setAction(A_MIMROCK_WALK);
-		    else
-			setAction(A_MIMROCK_SIT);
-		}
-		
-	    }
-	
+    {
+        const int dx = player->getXMidPos() - getXMidPos();
+
+        if( dx>-CSF_DISTANCE_TO_FOLLOW_TOLERANCE &&
+                dx<+CSF_DISTANCE_TO_FOLLOW_TOLERANCE )
+        {
+            if( dx<0 )
+                xDirection = LEFT;
+            else
+                xDirection = RIGHT;
+
+            if( !getProbability(40) )
+                return true;
+
+            if( dx>-CSF_DISTANCE_TO_JUMP_TOLERANCE &&
+                    dx<+CSF_DISTANCE_TO_JUMP_TOLERANCE )
+            {
+                if( xDirection == -player->xDirection )
+                {
+                    setAction(A_MIMROCK_SIT);
+                }
+                else
+                {
+                    yinertia = -JUMP_HEIGHT;
+                    mTimer = JUMP_TIME;
+
+                    setAction(A_MIMROCK_JUMP);
+                }
+            }
+            else
+            {
+                if( xDirection == player->xDirection && !blockedr && !blockedl )
+                {
+                    setAction(A_MIMROCK_WALK);
+                }
+                else
+                {
+                    setAction(A_MIMROCK_SIT);
+                }
+            }
+        }
+
     }
     
     return true;
