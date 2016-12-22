074b3418ef628c349d14970706f3a1198b551219
Gerhard Stein <stein@leat.rub.de>
2016-12-22 20:31:40 +0100

    Added nice python scripts for Keen 9

---
+++ vfsroot/scripts/Keen9/ai/ampton.py
@@ -0,0 +1,15 @@
+def healthPoints():
+    # Number of hit points before enemy is stunned
+    return 3
+
+def mayShoot():
+    # Number of hit points before enemy is stunned
+    return True
+
+def screamAfterShoot():
+    # Number of hit points before enemy is stunned
+    return False
+
+
+def walkSound():
+    return 0
---
+++ vfsroot/scripts/Keen9/ai/qed.py
@@ -0,0 +1,23 @@
+def getLevelText(level):   
+       if level == 5:
+          text =  "Princess Lindsey says:\n"
+          text += "You won't be able to \n"
+          text += "defeat Mortimer with\n"
+          text += "your stunner alone!"
+       elif level == 11:
+          text =  "Lt. Barker says:\n"
+          text += "Beware of the\n"
+          text += "Krodacian Overlords!"
+       elif level == 2:
+          text =  "Spot says:\n"
+          text += "The Shikadi are\n"
+          text += "invincible while\n"
+          text += "climbing poles!" 
+       else:
+          text =  "Oracle Janitor says:\n"
+          text += "There's something\n"
+          text += "mysterious beneath the\n"
+          text += "giant crater."
+       return text
+
+
