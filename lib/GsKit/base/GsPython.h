310fb76087603c29a978b333e2f81fbc6fab0eb5
Gerhard Stein <stein@leat.rub.de>
2016-12-28 11:14:15 +0100

    Added missing Python classes

---
+++ lib/GsKit/base/GsPython.h
@@ -0,0 +1,31 @@
+#ifndef GSPYTHON_H
+#define GSPYTHON_H
+
+#include <Python.h>
+#include <base/Singleton.h>
+#include <string>
+
+#define PYTHON_VERBOSE 0
+
+#define gPython	GsPython::get()
+
+class GsPython : public GsSingleton<GsPython>
+{
+
+public:
+    /**
+     * @brief loadModule  Load the module and return as python object
+     * @param scriptBaseName    name of script to load
+     * @param baseDir           Base directory of the game where to look at
+     * @return pointer to the module
+     */
+    PyObject *loadModule(const std::string &scriptBaseName,
+                         const std::string &baseDir);
+
+
+};
+
+
+
+
+#endif // GSPYTHON_H
