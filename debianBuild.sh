80e12aedd1ed3de2d00332d49e4b8f879180e4b4
Gerhard Stein <stein@leat.rub.de>
2017-01-14 15:42:10 +0100

    Automated debian/ppa way to build CG for Ubuntu

---
+++ debianBuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+VERSION=1.9.8
+KEY="0x6A1F4CC6"
+
+DST=../cgenius_${VERSION}
+
+echo "Preparing assets for directory..."
+
+# Be careful, not everything can be copied!
+mkdir -p ${DST}
+cp -r CMake ${DST}
+cp -r debian ${DST}
+cp -r docs ${DST}
+cp -r lib ${DST}
+cp -r share ${DST}
+cp -r src ${DST}
+cp -r tools ${DST}
+cp -r vfsroot ${DST}
+cp -r share ${DST}
+cp *.* ${DST}
+
+# Build it the debian way
+cd ${DST}
+
+debuild -S -k${KEY}
+
+debuild -us -uc
+#dput ppa:gerstrong/cgenius commandergenius_${VERSION}ppa1_source.changes
