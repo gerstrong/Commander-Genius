359a23c6b617243bc9f3c13c837620528687026c
Gerhard Stein <gerstrong@steinzone.de>
2017-09-23 10:13:01 +0200

    Time for some Read/get changes

--- GsKit/base/TypeDefinitions.h
+++ GsKit/base/TypeDefinitions.h
@@ -10,6 +10,8 @@
 #ifndef __TYPEDEFINITIONS_H__
 #define __TYPEDEFINITIONS_H__
 
+// Some good basic types
+
 #include <SDL.h>
 
 typedef uint8_t byte;
@@ -18,35 +20,65 @@ typedef int32_t fixed;
 typedef uint32_t longword;
 typedef void * memptr;
 
-static inline word READWORD(byte *&ptr)
+
+/**
+ * @brief GETWORD Extracts a word (16-bit integer) from a byte pointer.
+ *                It endian independent, assuming we a reading data from
+ *                a little endian system
+ * @param ptr     Pointer to data
+ * @return        The value
+ */
+static inline auto GETWORD(byte *ptr) -> word
 {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	word val = ptr[0]<<8 | ptr[1];
+    return (ptr[0]<<8 | ptr[1]);
 #else
-	word val = ptr[0] | ptr[1] << 8;
+    return (ptr[0] | ptr[1] << 8);
 #endif
+}
+
+/**
+ * @brief READWORD Like GETWORD but the pointer will also be modified, by
+ *                 going two bytes forward
+ * @param ptr      reference to pointer (*&) which will be modified
+ * @return         the value
+ */
+static inline auto READWORD(byte *&ptr) -> word
+{
+    const auto val = GETWORD(ptr);
     ptr += sizeof(word);
     return val;
 }
 
-static inline word GETWORD(byte *ptr){
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	return (ptr[0]<<8 | ptr[1]);
-#else
-	return (ptr[0] | ptr[1] << 8);
-#endif
-}
 
-static inline longword READLONGWORD(byte *&ptr)
+/**
+ * @brief GETLONGWORD   Like GETWORD but 4 bytes.
+ * @param ptr           Pointer to data
+ * @return              The value
+ */
+static inline auto GETLONGWORD(byte *ptr) -> longword
 {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	longword val = ptr[0]<<24 | ptr[1] << 16 | ptr[2] << 8 | ptr[3];
+    longword val = ptr[0]<<24 | ptr[1] << 16 | ptr[2] << 8 | ptr[3];
 #else
-	longword val = ptr[0] | ptr[1] << 8 | ptr[2] << 16 | ptr[3] << 24;
+    longword val = ptr[0] | ptr[1] << 8 | ptr[2] << 16 | ptr[3] << 24;
 #endif
+    return val;
+}
+
+
+/**
+ * @brief READLONGWORD  Like READWORD but 4 bytes
+ * @param ptr      reference to pointer (*&) which will be modified
+ * @return         the value
+ */
+static inline auto READLONGWORD(byte *&ptr) -> longword
+{
+    const auto val = GETLONGWORD(ptr);
     ptr += 4;
     return val;
 }
+
 
 
 #endif /* __TYPEDEFINITIONS_H__ */
