a3bea486245d075b0ac5daf2854f6ce24a8905eb
Gerhard Stein <gerstrong@gmail.com>
2018-07-08 06:58:50 +0200

    Removed the VPad configurator. Not yet ready for production

--- src/engine/CGameLauncher.cpp
+++ src/engine/CGameLauncher.cpp
@@ -157,12 +157,13 @@ bool CGameLauncher::setupMenu()
                                              0.75f ),
                                GsRect<float>(0.0f, 0.0f, 0.069f, 0.069f) );
 
-    mLauncherDialog.addControl(new GsButton( "VPad", new OpenVGamePadSettingsEvent(),
+    /*mLauncherDialog.addControl(new GsButton( "VPad", new OpenVGamePadSettingsEvent(),
                                              GsControl::Style::NONE,
                                              0.75f,
                                              1.0f,
                                              1.0f ),
                                GsRect<float>(0.75f, 0.0f, 0.17f, 0.069f) );
+                               */
 
     mLauncherDialog.addControl(new GsButton( "|", new OpenSettingsMenuEvent(),
                                              GsControl::Style::NONE,
