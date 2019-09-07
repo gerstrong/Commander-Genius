6d6a009e831574eae45127aff5f050fe8aa24859
gerstrong@gmail.com <Gerhard Stein>
2019-09-07 11:53:49 +0200

    Version update

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef CG_VERSION_H
 #define CG_VERSION_H
 
-#define CGVERSION       "2.3.5-Release"
-#define CGVERSIONCODE	235000
+#define CGVERSION       "2.3.6-Release"
+#define CGVERSIONCODE	236000
 
 #endif
