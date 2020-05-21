42cbfc1948056a5632127a773511828d9b77e09f
Gerhard Stein <gerhard@steinzone.de>
2020-05-21 11:26:16 +0200

    Version update, roadmap preparation

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef CG_VERSION_H
 #define CG_VERSION_H
 
-#define CGVERSION       "2.4.2-Release"
-#define CGVERSIONCODE	242000
+#define CGVERSION       "2.4.3-Release"
+#define CGVERSIONCODE	243000
 
 #endif
