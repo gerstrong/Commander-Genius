6d6631883875381d408a6f306109aa74f32d29f8
Gerhard Stein <gerhard@steinzone.de>
2020-08-13 10:48:19 +0200

    Version update

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef CG_VERSION_H
 #define CG_VERSION_H
 
-#define CGVERSION       "2.5.1-Release"
-#define CGVERSIONCODE	251000
+#define CGVERSION       "2.5.2-Release"
+#define CGVERSIONCODE	252000
 
 #endif
