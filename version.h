f92cb65396fbade2a3f8f5c0b9a06b37df725d73
Gerhard Stein <gerhard@steinzone.de>
2020-10-25 19:03:31 +0100

    Version update and CMake fixes so it works on older version

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef CG_VERSION_H
 #define CG_VERSION_H
 
-#define CGVERSION       "2.6.2-Release"
-#define CGVERSIONCODE	262000
+#define CGVERSION       "2.6.3-Release"
+#define CGVERSIONCODE	263000
 
 #endif
