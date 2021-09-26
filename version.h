c85adba11fa411b2c2c568eae5deed38b7ce38a8
Gerhard Stein <gerstrong@gmail.com>
2021-09-26 12:52:28 +0200

    Bump Version

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef CG_VERSION_H
 #define CG_VERSION_H
 
-#define CGVERSION       "3.0.4-Release"
-#define CGVERSIONCODE	304000
+#define CGVERSION       "3.0.5-Release"
+#define CGVERSIONCODE	305000
 
 #endif
