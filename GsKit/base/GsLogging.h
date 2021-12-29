dceae03735125c2f73d2ece7e4d76c9320cc84c3
Gerhard Stein <gerstrong@gmail.com>
2021-12-29 11:33:00 +0100

    Naming improvements

--- GsKit/base/GsLogging.h
+++ GsKit/base/GsLogging.h
@@ -1,12 +1,12 @@
 /*
- * CLogFile.h
+ * GsLogging.h
  *
  *  Created on: 20.04.2009
  *      Author: gerstrong
  */
 
-#ifndef CLOGFILE_H_
-#define CLOGFILE_H_
+#ifndef GSLOGGING_H
+#define GSLOGGING_H
 
 #include <base/Singleton.h>
 #include <string>
@@ -19,20 +19,20 @@
 
 enum class FONTCOLORS
 {
-	BLACK,
-	RED,
-	GREEN,
-	BLUE,
-	PURPLE
+    BLACK,
+    RED,
+    GREEN,
+    BLUE,
+    PURPLE
 };
 
 class CLogFile : public GsSingleton<CLogFile>
 {
 public:
-	
-	CLogFile();
-	virtual ~CLogFile();
-	
+
+    CLogFile();
+    virtual ~CLogFile();
+
     /**
      * @brief CreateLogfile     Open the file for creating some content of general information like
      *                          about the system and the application.
@@ -49,25 +49,25 @@ public:
 
     void closeIt();
 
-	void WriteTopic		(const char *Topic, int Size);
-	void textOut		(const std::string& Text);
-	void textOut		(FONTCOLORS Color, const std::string& Text);
-	void textOut		(FONTCOLORS Color, bool List, const std::string& Text);
-	void ftextOut		(const char *Text, ...);
-	void ftextOut		(FONTCOLORS Color, const char *Text, ...);
-	void fltextOut		(FONTCOLORS Color, bool List, const char *Text, ...);
-	void FunctionResult	(const char *Name, bool Result);
+    void WriteTopic		(const char *Topic, int Size);
+    void textOut		(const std::string& Text);
+    void textOut		(FONTCOLORS Color, const std::string& Text);
+    void textOut		(FONTCOLORS Color, bool List, const std::string& Text);
+    void ftextOut		(const char *Text, ...);
+    void ftextOut		(FONTCOLORS Color, const char *Text, ...);
+    void fltextOut		(FONTCOLORS Color, bool List, const char *Text, ...);
+    void FunctionResult	(const char *Name, bool Result);
 
     CLogFile & operator << (const char *txt);
     CLogFile & operator << (const std::string &str);
-    CLogFile & operator << (const int val);    
+    CLogFile & operator << (const int val);
 
     static const char *endl;
 
 private:
 
     std::ofstream mLogStream;
-	
+
     std::string removeHTML(const std::string& input);
 
     std::array<char, 1024> buffer;
